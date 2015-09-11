25bf960f3a4cb761808919149ae1c69a062977b2
Chris Blake <chris@code.sc>
2015-09-11 14:59:49 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,61 @@
+PinCer
+======
+**P**.in**C**.er stands for "**P**uny **S**erver". Yes, I know.
+
+PinCer is a recursive static file server that serves content-types correctly
+and logs nicely that runs on Node.js.
+
+Its primary purpose is iteratively developing and debugging entirely static
+websites.
+
+(**Don't use PinCer in production environments.**).
+
+Usage Examples
+--------------
+Serve the current directory @ `localhost:8000`:
+
+        $ pincer
+
+Serve the directory `WEBROOT` @ `localhost:8000`:
+
+        $ pincer [WEBROOT]
+
+Serve the directory `/var/www` @ `0.0.0.0:8080`:
+
+        $ CWDTS=/var/www HOSTTS=0.0.0.0 PORTTS=8080 pincer
+
+`CWDTS` defaults to the current directory.
+`HOSTTS` defaults to `localhost`.
+`PORTTS` defaults to `8000`.
+
+N.B.: The `WEBROOT` argument takes precedence over the `CWDTS` environment
+variable, if both are specified.
+
+Show pincer's help message:
+        $ pincer [ --help | -h | --h ]
+
+Show the current version of pincer:
+        $ pincer [ --version | -v | --v ]
+
+Install
+-------
+    $ sudo npm install -g pincer
+
+Why does this exist?
+--------------------
+Because I was tired of writing the same static file server over and over again.
+Serving static files with express.js is nice, but it's like killing a bug with
+a forest fire.
+
+pincer ins one-file small (~100 lines, excluding dependencies) and has super
+sane defaults.
+
+Features
+--------
+- Recursive static file serving.
+- Serves Content-Types based on file extension (defaulting to `text/plain` if
+  non-existent, or unknown)
+- Logs requested server route, and the correspondingly served file.
+	- Logs file serving errors as well (non-existent file, etc.).
+- Serves files with the `utf-8` charset.
+- One-file (~100 lines, excluding dependencies) small, so super extensible.
---
+++ index.js
@@ -0,0 +1,117 @@
+'use strict';
+
+process.argv.forEach(function argParser(arg) {
+	if (arg === '--help' || arg === '-h' || arg === '--h') {
+		console.log(
+			'\n' +
+			'pincer - recursive static file server\n' +
+			'\n' +
+			'Usage Examples:\n' +
+			'\n' +
+			'Serve the current directory @ localhost:8000:\n' +
+			'\n' +
+			'	$ pincer\n' +
+			'\n' +
+			'Serve the directory WEBROOT @ localhost:8000:\n' +
+			'\n' +
+			'	$ pincer [WEBROOT]\n' +
+			'\n' +
+			'Serve the directory "/var/www" @ 0.0.0.0:8080:\n' +
+			'\n' +
+			'	$ CWDTS=/var/www HOSTTS=0.0.0.0 PORTTS=8080 pincer\n' +
+			'\n' +
+			'CWDTS defaults to the current directory.\n' +
+			'HOSTTS defaults to "localhost".\n' +
+			'PORTTS defaults to "8000".\n' +
+			'\n' +
+			'N.B.: The WEBROOT argument takes precedence over the CWDTS environment variable, if both are specified.\n' +
+			'\n' +
+			'-----------------------\n' +
+			'\n' +
+			'Show pincer\'s help message:\n' +
+			'	$ pincer [ --help | -h | --h ]\n' +
+			'\n' +
+			'Show the current version of pincer:\n' +
+			'	$ pincer [ --version | -v | --v ]\n'
+		);
+		return process.exit(0);
+	}
+
+	if (arg === '--version' || arg === '-v' || arg === '--v') {
+		var VERSION = require('./package.json').version;
+		console.log('pincer version %s', VERSION);
+		return process.exit(0);
+	}
+});
+
+var fs   = require('fs');
+var http = require('http');
+var path = require('path');
+var url  = require('url');
+var util = require('util');
+
+var mime = require('mime');
+
+mime.default_type = mime.lookup('txt');
+
+var ENOENT            = 'ENOENT';
+var TEXT_CONTENT_TYPE = {'Content-Type': mime.lookup('txt') + '; charset=utf-8'};
+
+var cwd  = process.env.TSCWD  || process.argv[2] || process.cwd();
+var host = process.env.TSHOST || 'localhost';
+var port = process.env.TSPORT || 8000;
+
+var server = http.createServer(function handler(request, response) {
+	response.sendResponse = function sendResponse(code, header, body) {
+		this.writeHead(code, header);
+		return this.end(body);
+	}
+
+	var pathname = url.parse(request.url).pathname;
+	var file = path.join(cwd, pathname.substring(1));
+
+	console.log('Request for "%s" ===>  Mapped to "%s".', request.url, file);
+
+	fs.stat(file, function stat(err, stat) {
+		if (err) {
+			if (err.code === ENOENT) {
+				console.log('\tFile "%s" does not exist.', file);
+				return response.sendResponse(404, TEXT_CONTENT_TYPE, 'File "' + file + '" does not exist.');
+			}
+
+			console.log('\tSome error occured when trying to find file "%s".',
+				file);
+			return response.sendResponse(500, TEXT_CONTENT_TYPE,
+				'Some error occured when trying to find file "' + file + '". The error is as follows:\n' +
+				util.inspect(err, {depth: null}));
+		}
+
+		if (stat.isDirectory()) {
+			fs.readdir(file, function readdir(err, files) {
+				if (err) {
+					console.log('\tSome error occured when trying to list directory contents of  "%s".', file);
+					return response.sendResponse(500, TEXT_CONTENT_TYPE,
+						'Some error occured when trying to list directory contents of "' +  file + '". The error is as follows:\n' +
+						util.inspect(err, {depth: null}));
+				}
+
+				console.log('\tListing directory contents of "%s".', file);
+				var fileInDir;
+				files.forEach(function link(file) {
+					fileInDir = path.join(pathname, file);
+					response.write(fileInDir.link(fileInDir) + '<br>\n');
+				});
+				return response.end();
+			});
+		} else {
+			response.writeHead(200,
+				{'Content-Type': mime.lookup(file) + '; charset=utf-8'});
+			return fs.createReadStream(file).pipe(response);
+		}
+	});
+});
+
+server.listen(port, host, function listener() {
+	console.log('Server listening @ %s:%d with webroot as "%s".',
+		host, port, cwd);
+});
